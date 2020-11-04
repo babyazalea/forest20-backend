8e99044601609f21eed157f15be7210938c1d57e
babyazalea <usun12@hotmail.com>
2020-11-04 23:57:24 +0900

    conneted db

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
 import morgan from "morgan";
 import helmet from "helmet";
+import path from "path";
 import routes from "./routes";
 import globalRouter from "./router/globalRouter";
 import adminRouter from "./router/adminRouter";
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes.home, globalRouter);
 app.use(routes.admin, adminRouter);
@@ -17,5 +20,19 @@ app.use(routes.admin, adminRouter);
 const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 };
+
+mongoose.connect("mongodb://localhost:27017/forest20", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleConnectDB = () => console.log("Connected to DB");
+const handleErrorDB = () => console.log(`Error On DB Connection:${error}`);
+
+db.once("open", handleConnectDB);
+db.on("error", handleErrorDB);
 
 app.listen(PORT, handleListening);
