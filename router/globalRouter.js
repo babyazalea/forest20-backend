7809ef20741af4043cf723a0a5e8d65756f21489
babyazalea <usun12@hotmail.com>
2020-11-12 00:11:11 +0900

    C:/Program Files/Git/public 추가

--- router/globalRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import routes from "../routes";
-import { getHome } from "../controller/globalController";
-
-const globalRouter = express.Router();
-
-globalRouter.get(routes.home, getHome);
-
-export default globalRouter;
